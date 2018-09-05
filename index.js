1f5b12efcaae69b46235e1a89632187665549308
AndyBarnettCTM <andrew.barnett@bglgroup.co.uk>
2018-09-05 14:28:09 +0100

    beginnings of collision

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ kontra.assets.load('character_walk_sheet.png').then(function() {
     });
 
 let sprite = kontra.sprite({
-  x: 100,        // starting x,y position of the sprite
-  y: 98,
+  x: 0,        // starting x,y position of the sprite
+  y: 249,
   animations: spriteSheet.animations,
   //dx: 2,          // move the sprite 2px to the right every frame
   update: function() {
@@ -26,7 +26,9 @@ let sprite = kontra.sprite({
     }
     else if (kontra.keys.pressed('right')) {
       // right arrow pressed
-      if(this.x + this.width < kontra.canvas.width) this.x++;
+      if(!isCollision(this.x, this.y, terrain) && (this.x + this.width < kontra.canvas.width)) {
+          this.x++;
+      }
     }
 
     if (kontra.keys.pressed('up')) {
@@ -35,33 +37,58 @@ let sprite = kontra.sprite({
     }
     else if (kontra.keys.pressed('down')) {
       // down arrow pressed
-      if (this.y <= (kontra.canvas.height - sprite.height)) this.y++;
+      console.log (isCollision(this.x, this.y, terrain));
+      if (!isCollision(this.x, this.y, terrain) && (this.y <= (kontra.canvas.height - sprite.height))) {
+          // allow downward movement
+          this.y++;
+      }
     }
   }
 });
 
-let isCollision = function(targetX, targetY){
+let isCollision = function(targetX, targetY, terrain){
+    let blockNumber = 0
+    let collision = false;
+    for (var terrainBlock of terrain) {
+        //console.log('calculating terrain' + terrainBlock);
+        //if(targetY >= (kontra.canvas.height - 50 * terrainBlock,50,50*terrainBlock)){
+        let terrainBlockXOrig = (blockNumber * 50);
+        let terrainBlockXEnd = (terrainBlockXOrig + 50);
+        let terrainBlockYEnd = (kontra.canvas.height - ((terrainBlock) * 50))
+
+        //console.log(targetX, terrainBlockXEnd);
+        if (targetY >= terrainBlockYEnd) {
+            collision = true;
+            //console.log('Y collision!');
+        }
+        if (targetX >= terrainBlockXEnd) {
+            collision = true;
+            //console.log('X collision!');
+        }
+        blockNumber++;
+    return collision;
+    };
 
     return false;
 };
 
 sprite.playAnimation('walk');
-let terrain = [0,1,2,4,2,2,1,0,0,0,1,2];
+let terrain = [1,1,2,4,2,2,1,0,0,0,1,2];
 let ctx = kontra.canvas.getContext("2d");
 let loop = kontra.gameLoop({
   update: function() {        // update the game state
     sprite.update();
 
-    // wrap the sprites position when it reaches
+    // stop the sprite's position when it reaches
     // the edge of the screen
     if (sprite.y <= (kontra.canvas.height - sprite.height)) {
-        if(!isCollision(sprite.x,sprite.y+1))
-        {
-            sprite.y++;
-        }
+        // if(!isCollision(sprite.x, sprite.y, terrain))
+        // {
+        //     //sprite.y++;
+        // }
     }
   },
-  render: function() {  
+  render: function() {
           // render the game state
           let index = 0;
           var my_gradient=ctx.createLinearGradient(0,0,0,150);
@@ -78,4 +105,3 @@ let loop = kontra.gameLoop({
 
 loop.start();    // start the game
 });
-
